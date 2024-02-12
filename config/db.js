a9329e98a63a7ad13ca791b4cd7c4d90ac01acfc
DMenace92 <dennisenwiya@gmail.com>
2024-02-12 14:29:02 -0700

    diploy test

--- config/db.js
+++ config/db.js
@@ -5,14 +5,16 @@ const config = require('../config/config')
 
 let newConfig = config;
 
-mongoose.connect(newConfig, {
-    useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
-    useUnifiedTopology: true,
+// mongoose.connect(newConfig, {
+//     useNewUrlParser: true,
+//     // useCreateIndex: true,
+//     // useFindAndModify: false,
+//     useUnifiedTopology: true,
 
 
-})
+// })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
 mongoose.connection.on('connected', ()=>{
     console.log('connected to mongodb')
 })
